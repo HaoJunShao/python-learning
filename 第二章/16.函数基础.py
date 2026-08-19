cf78e6e9e6e56dfe684b5327a23c81aaacb1ccdd
HaoJunShao <shj080425@icloud.com>
2026-08-19 20:09:14 +0800

    函数案例

--- 第二章/16.函数基础.py
+++ 第二章/16.函数基础.py
@@ -4,29 +4,44 @@
 # Xu()
 
 
-def circle_area(r):
-    return 3.14 * (r ** 2)
-print(circle_area(10))
+# def circle_area(r):
+#     return 3.14 * (r ** 2)
+# print(circle_area(10))
+#
+# def rectangle_area(l,w):
+#     """
+#
+#     :param l:
+#     :param w:
+#     :return:长方形的面积
+#     """
+#     return l*w
+# print(rectangle_area(5,6))
+#
+#
+# def circle_area_length(r):
+#     """
+#     根据圆的半径，计算圆的面积和周长
+#     :param r: 圆的半径
+#     :return: 圆的面积，圆的周长
+#     """
+#     return 3.14 * r ** 2,round(2*3.14*r,1)
+# a,b = circle_area_length(5)
+# print(a)
+# print(b)
+# help(circle_area_length)
 
-def rectangle_area(l,w):
-    """
 
-    :param l:
-    :param w:
-    :return:
-    """
-    return l*w
-print(rectangle_area(5,6))
+def function_a():
+    print(1)
+    function_b()
+    print(5)
+def function_b():
+    print(2)
+    function_c()
+    print(4)
+def function_c():
+    print(3)
+function_a()
+print("函数调用完毕")
 
-
-def circle_area_length(r):
-    """
-    根据圆的半径，计算圆的面积和周长
-    :param r: 圆的半径
-    :return: 圆的面积，圆的周长
-    """
-    return 3.14 * r ** 2,round(2*3.14*r,1)
-a,b = circle_area_length(5)
-print(a)
-print(b)
-help(circle_area_length)
---
+++ 第二章/17.函数案例.py
@@ -0,0 +1,92 @@
+# def area_triangle(d,h):
+#     area = d * h /2
+#     return round(area,1)
+# m = area_triangle(3.1435,3)
+# print(m)
+
+
+# m = input("请输入单词:")
+# def cou(m):
+#     aa = m.count("a")
+#     ee = m.count("e")
+#     ii = m.count("i")
+#     oo = m.count("o")
+#     uu = m.count("u")
+#     AA = m.count("A")
+#     EE = m.count("E")
+#     II = m.count("I")
+#     OO = m.count("O")
+#     UU = m.count("U")
+#     bo = aa + ee +ii + oo + uu +AA +EE +II +OO +UU
+#     return bo
+# print(cou(m))
+
+
+# m = input("请输入单词:")
+# def cou(m):
+#     s = 0  #函数默认调用局部变量，变量需要在函数内部定义！
+#     for i in m:
+#         if i == "a" or i == "e" or i == "i" or i == "o" or i == "u" or i == "A" or i == "E" or i == "I" or i == "O":
+#             s = s + 1
+#     return s
+# print(cou(m))
+
+
+# m = int(input("请输入成绩："))
+# def function_a(m):
+#     print(max(m))
+#     print(min(m))
+# function_a(m)
+
+# s1 = [666,677,678,633,647]
+# def function_a(m):
+#     print("最大值是：",max(m))
+#     print(min(m))
+#     print(round(sum(m)/len(m),1))
+# function_a(s1)
+
+
+# m = float(input("请输入数字："))
+# def function_a(n):
+#     if n >= 90:
+#         print("A")
+#     elif n>= 75:
+#         print("B")
+#     else:
+#         print("C")
+# function_a(m)
+
+
+# def fun(m):
+#     if m == m[::-1]:
+#         print("True")
+#     else:
+#         print("False")
+# n = input("请输入字符串：")
+# fun(n)
+
+
+# def timing(f):
+#     a = f // 3600
+#     b = f % 3600 // 60
+#     c = f - a * 3600 - b * 60
+#     print(a,"小时",b,"分",c,"秒")
+# m = int(input("请输入时间："))
+# timing(m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
